2fd841eb1dab16ff9bd12fb79a0c64e644d3fd67
oscar <oschneider742@gmail.com>
2026-06-05 14:58:31 -0300

    feat:se creo la funcion para construir las cartas

--- asset/js/app.js
+++ asset/js/app.js
@@ -20,6 +20,23 @@ const construir_cuerpo = (lista_personajes) => {
     contenedorCartas.innerHTML = "";
 
     lista_personajes.forEach(personaje => {
+        contenedorCartas.innerHTML += `
+
+        <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" class=" card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${personaje.name}</h5>
+                            <p class="card-text">${personaje.occupation}</p>
+                            <p class="${personaje.status === 'Alive'? 'card-text badge text-success bg-light' : 'card-text badge text-danger bg-light'}">${personaje.status}</p>
+                            <hr>
+                            <a href="#" class="btn btn-warning" data-id="${personaje.id}">Ver Detalle</a>
+                        </div>
+                    </div>
+                </div>
+
+        `;
+
         console.log(personaje);
     })
 }
