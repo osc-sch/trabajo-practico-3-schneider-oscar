2d96303d182cb1676532559e8d7df6f707becb5e
oscar <oschneider742@gmail.com>
2026-06-05 13:50:02 -0300

    Style:Se definio la estructura base de la aperiencia del sitio web

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,3 +1,6 @@
 const urlTodosLosPersonajes = 'https://thesimpsonsapi.com/api/characters';
 const urlUnPersonaje = 'https://thesimpsonsapi.com/api/characters/';
 
+fetch(urlTodosLosPersonajes)
+    .then(result => result.json())
+    .then(data => console.log(data.results[0]))
