3fff6305fb8e1bab209a0de80a4534ba9d99051d
oscar <oschneider742@gmail.com>
2026-06-05 23:37:12 -0300

    feat:se creo la funcion para buscar por nombre del personaje

--- asset/js/app.js
+++ asset/js/app.js
@@ -2,6 +2,9 @@ const urlTodosLosPersonajes = 'https://thesimpsonsapi.com/api/characters';
 const urlUnPersonaje = 'https://thesimpsonsapi.com/api/characters/';
 
 const contenedorCartas = document.querySelector('#contenedor_cards');
+const buscador = document.querySelector('#buscador');
+const btnBuscar = document.querySelector('#button_buscar');
+const btnLimpiar = document.querySelector('#btn-limpiar');
 
 let personajes = [];
 
@@ -21,31 +24,31 @@ const construir_cuerpo = (lista_personajes) => {
 
     contenedorCartas.innerHTML = "";
 
-    if(lista_personajes.results){
-        lista_personajes.results.forEach(personaje => {
+   /* if(lista_personajes.results){*/
+        lista_personajes.forEach(personaje => {
        
         contenedorCartas.innerHTML += `
 
-        <div class="col-3">
-                    <div class="card" style="width: 18rem;">
-                        <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" class=" card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${personaje.name}</h5>
-                            <p class="card-text">${personaje.occupation}</p>
-                            <p class="${personaje.status === 'Alive'? 'card-text badge text-success bg-light' : 'card-text badge text-danger bg-light'}">${personaje.status}</p>
-                            <hr>
-                            <a href="#" class="btn btn-warning" data-id="${personaje.id}">Ver Detalle</a>
-                        </div>
-                    </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="https://cdn.thesimpsonsapi.com/500${personaje.portrait_path}" class=" card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${personaje.name}</h5>
+                    <p class="card-text">${personaje.occupation}</p>
+                    <p class="${personaje.status === 'Alive'? 'card-text badge text-success bg-light' : 'card-text badge text-danger bg-light'}">${personaje.status}</p>
+                    <hr>
+                    <a href="#" class="btn btn-warning" data-id="${personaje.id}">Ver Detalle</a>
                 </div>
+            </div>
+        </div>
 
         `;
 
-    })
+    })/*
     }else{
         contenedorCartas.innerHTML += `
 
-        <div class="col-3">
+        <div class="col">
                     <div class="card" style="width: 18rem;">
                         <img src="https://cdn.thesimpsonsapi.com/500${lista_personajes.portrait_path}" class=" card-img-top" alt="...">
                         <div class="card-body">
@@ -59,13 +62,25 @@ const construir_cuerpo = (lista_personajes) => {
                 </div>
 
         `;
-        console.log(lista_personajes);
+        console.log(lista_personajes);*/
     }
+    
+btnLimpiar.addEventListener('click',()=>{
+    buscador.value ='';
+    construir_cuerpo(personajes.results);
+})
 
-}
 
 document.addEventListener('DOMContentLoaded', async () => {
     personajes = await obtener_todos_personajes();
 
-    construir_cuerpo(personajes);
+    construir_cuerpo(personajes.results);
+})
+
+buscador.addEventListener('input',() => {
+
+    const buscar = personajes.results.filter(personaje => personaje.name.toLowerCase().includes(buscador.value));
+
+    construir_cuerpo(buscar);
+    console.log(buscar);
 })
